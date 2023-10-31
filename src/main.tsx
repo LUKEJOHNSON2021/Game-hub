908690e2ba7d5ae3098949d0b255bfc0c4a1879a
LUKEJOHNSON2021 <LUKE.JOHNSON-2021@hull.ac.uk>
2023-10-31 18:57:56 +0000

    Implement Searching

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import ReactDOM from "react-dom/client";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 
 import App from "./App";
+import "./index.css";
 import theme from "./theme";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
