7243ab86925b74cc8e5af8c3f2d3338511a424d3
LUKEJOHNSON2021 <LUKE.JOHNSON-2021@hull.ac.uk>
2023-11-05 13:07:54 +0000

    handle errors

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,12 +1,16 @@
 import { createBrowserRouter } from "react-router-dom";
+
+//pages
 import Layout from "./Pages/Layout";
 import HomePage from "./Pages/HomePage";
 import GameDetailPage from "./Pages/GameDetailPage";
+import ErrorPage from "./Pages/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
+    errorElement: <ErrorPage />,
     children: [
       {
         index: true,
