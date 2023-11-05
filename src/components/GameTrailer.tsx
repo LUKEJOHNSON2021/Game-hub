5843a11ffff0893c5df1ffcb65182867a00936a7
LUKEJOHNSON2021 <LUKE.JOHNSON-2021@hull.ac.uk>
2023-11-05 15:29:49 +0000

    build game trailer

---
+++ src/components/GameTrailer.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import useTrailers from "../hooks/useTrailers";
+
+interface Props {
+  gameId: number;
+}
+
+const GameTrailer = ({ gameId }: Props) => {
+  const { data, error, isLoading } = useTrailers(gameId);
+
+  const first = data?.results[0];
+  if (error) throw error;
+
+  return first ? (
+    <video src={first.data[480]} poster={first.preview} controls />
+  ) : null;
+};
+
+export default GameTrailer;
